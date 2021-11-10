7425d6af47b1ddc55552e333fa3ef2f5a42b83bf
Yigao LI <yigaol2@uw.edu>
2021-11-10 11:40:29 -0800

    commit

--- datafilter.py
+++ datafilter.py
@@ -3,7 +3,7 @@ import re
 from tqdm import tqdm
 
 
-JsonFile = open('naturalproofs_proofwiki.json', 'r')
+JsonFile = open('naturalproofs_proofwiki1.json', 'r')
 js = json.load(JsonFile)
 
 for i in tqdm(range(len(js['dataset']['theorems']))):
@@ -15,11 +15,12 @@ for i in tqdm(range(len(js['dataset']['theorems']))):
         for k in range(1, len(js['dataset']['theorems'][i]['proofs'][j]['contents'])):
             item = js['dataset']['theorems'][i]['proofs'][j]['contents'][k]
             x = item[0].isupper()
-            if x:
+            y = item[0: 2]
+            if x | (y == "{{") & (item != "{{begin-eqn}}"):
                 content.append(prev)
                 prev = item
             else:
-                prev = prev + item
+                prev = prev + " \n " + item
         js['dataset']['theorems'][i]['proofs'][j]['contents'] = content
 result = open("result.json", "w")
 result.write(json.dumps(js, indent=2))
