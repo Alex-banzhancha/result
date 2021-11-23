d7ac4f862e4c5ca8e1e04edde3d71651f5c4e2ba
Yigao LI <yigaol2@uw.edu>
2021-11-23 15:11:53 -0800

    adjust contents in theorems.

--- datafilter.py
+++ datafilter.py
@@ -3,25 +3,26 @@ import re
 from tqdm import tqdm
 
 
-JsonFile = open('naturalproofs_proofwiki1.json', 'r')
+JsonFile = open('naturalproofs_proofwiki2.json', 'r')
 js = json.load(JsonFile)
 
 for i in tqdm(range(len(js['dataset']['theorems']))):
-    for j in range(len(js['dataset']['theorems'][i]['proofs'])):
-        if js['dataset']['theorems'][i]['proofs'][j]['contents'] == []:
-            continue
-        prev = js['dataset']['theorems'][i]['proofs'][j]['contents'][0]
-        content = []
-        for k in range(1, len(js['dataset']['theorems'][i]['proofs'][j]['contents'])):
-            item = js['dataset']['theorems'][i]['proofs'][j]['contents'][k]
-            x = item[0].isupper()
-            y = item[0: 2]
-            if x | (y == "{{") & (item != "{{begin-eqn}}"):
-                content.append(prev)
-                prev = item
-            else:
-                prev = prev + " \n " + item
-        js['dataset']['theorems'][i]['proofs'][j]['contents'] = content
+    if js['dataset']['theorems'][i]['contents'] == []:
+        continue
+    prev = js['dataset']['theorems'][i]['contents'][0]
+    content = []
+    for k in range(1, len(js['dataset']['theorems'][i]['contents'])):
+        item = js['dataset']['theorems'][i]['contents'][k]
+        x = item[0].isupper()
+        y = item[0: 2]
+        if x | (y == "{{"):
+            content.append(prev)
+            prev = item
+        else:
+            prev = prev + " \n " + item
+    if len(content) == 0:
+        content.append(prev)
+    js['dataset']['theorems'][i]['contents'] = content
 result = open("result.json", "w")
 result.write(json.dumps(js, indent=2))
 JsonFile.close
